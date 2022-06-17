86f87ba9295079c2a0c831274586c45c4fcaf613
Borisbell <jmorrison@bk.ru>
2022-06-17 11:57:10 +0300

    cards errors update - delete card not present in bd

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,6 +30,7 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
+    .orFail(new Error('NotFound'))
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.message === 'NotFound') {
